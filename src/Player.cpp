4d4bc13f3b83ce54b15a2dee04bb39811bded84a
Gontijo8199 <rafael.gontijof2006@gmail.com>
2026-05-06 09:12:47 -0300

    build: Construção Inicial

---
+++ src/Player.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Player.hpp"
+
+Player::Player(int id, std::string name, int score, int timestamp){}
+Player::Player(){}
+Player::~Player(){}
+
+
+int Player::getId(){};
+std::string Player::getName(){};
+int Player::getScore(){};
+int Player::getTimestamp(){};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "Matchmaking.hpp"
+#include "Player.hpp"
+
+int main(){
+    
+    return 0;
+}
