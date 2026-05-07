5764297fdae8e96fa86f348abd514471da620828
Rudá Brandão <rudabrandao@ruoso.com>
2026-05-07 19:39:56 -0300

    feat: funções de teste

--- src/main.cpp
+++ src/main.cpp
@@ -4,109 +4,193 @@
 
 using namespace std;
 
-int main(){
+Matchmaking* gerar_jogo_ordenado(int n_players) {
+    Matchmaking* mm = new Matchmaking();
 
-/*
-    inserção de jogadores;
-    remoção de jogadores;
-    ordenação utilizando insertion sort;
-    ordenação utilizando merge sort;
-    ordenação com empate de score;
-    formação bem-sucedida de grupo;
-    tentativa de formação de grupo sem sucesso;
-    recuperação dos dados por meio do método getWaitingPlayers;
-    exibição do estado do sistema.
- */
-    cout << "Criando usuários..." << endl;
-    Player p1 = Player(1, "Ruda", 1000, 1);
-    Player p2 = Player(2, "Rafael", 1010, 2);
-    Player p3 = Player(3, "Maria", 1010, 3);
-    Player p4 = Player(4, "Matheus", 3000, 4);
-    Player p5 = Player(5, "Pedro", 500, 5);
+    Player p;
+    for (int i=0; i<n_players; i++) {
+        p = Player(i, "p"+to_string(i), (i+1)*10, i);
+        mm->insert(p);
+    }
 
-    Matchmaking* game = new Matchmaking();
+    return mm;
+}
 
-    cout << "Inserindo usuários..." << endl;
-    game->insert(p1);
-    game->insert(p3);
-    game->insert(p2);
-    game->insert(p4);
-    game->insert(p5);
+Matchmaking* gerar_jogo_desordenado(int n_players) {
+    Matchmaking* mm = new Matchmaking();
 
-    cout << "\nSalvando um backup_players..." << endl;
+    Player p;
+    for (int i=0; i<(n_players / 2); i++) {
+        p = Player(2*i, "p"+to_string(2*i), (i+1)*10, 2*i);
+        mm->insert(p);
+
+        p = Player(2*i + 1, "p"+to_string(2*i + 1), (i+1)*100, 2*i + 1);
+        mm->insert(p);
+    }
+
+    if (n_players%2 != 0) {
+        p = Player(n_players, "p"+to_string(n_players), (n_players+1)*10, n_players);
+        mm->insert(p);
+    }
+
+    return mm;
+}
+
+Matchmaking* gerar_jogo_desordenado_empatado(int n_players) {
+    Matchmaking* mm = new Matchmaking();
+
+    Player p;
+    for (int i=0; i<(n_players / 4); i++) {
+        p = Player(4*i, "p"+to_string(4*i), (i+1)*10, 4*i+1);
+        mm->insert(p);
+        p = Player(4*i+1, "p"+to_string(4*i+1), (i+1)*10, 4*i);
+        mm->insert(p);
+
+        p = Player(4*i+2, "p"+to_string(4*i+2), (i+1)*100, 4*i+3);
+        mm->insert(p);
+        p = Player(4*i+3, "p"+to_string(4*i+3), (i+1)*100, 4*i+2);
+        mm->insert(p);
+    }
+
+    int next = n_players/4; // 
+    for (int i=0; i<(n_players%4); i++) {
+        p = Player(4*next+i, "p"+to_string(4*next+i), (next+1)*10, n_players-i);
+        mm->insert(p);
+    }
+
+    return mm;
+}
+
+void buscar_grupo(int n_players, int g_size, int delta) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados com empate\n";
+
+    Matchmaking* mm = gerar_jogo_desordenado_empatado(n_players);
+    mm->printWaitingPlayers();
+    mm->sortByScoreMerge();
+
+    cout << "\nBuscando grupo com " << g_size << " jogadores e delta = " << delta << endl;
 
     int n;
-    Player* backup_players = game->getWaitingPlayers(&n);
+    Player* gp = mm->formGroup(g_size, delta, &n);
+    cout << "\nn esperado: " << g_size;
+    cout << "\nn obtido: " << n << endl;
 
-    game->printWaitingPlayers();
+    if (gp == nullptr)
+        cout << "Retorno de formGroup: nullptr" << endl;
+    else 
+        mm->printArrayPlayers(gp, n, "Grupo encontrado");
+    mm->printWaitingPlayers();
 
-    cout << "\nRemovendo usuário de ID=5..." << endl;
+    delete mm;
+    return;
+}
 
-    game->removePlayer(5);
-    game->printWaitingPlayers();
+//inserção ordenada
+void teste_1_1(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores ordenados\n";
+    Matchmaking* mm = gerar_jogo_ordenado(n_players);
 
-    delete game;
-    game = new Matchmaking();
-    for (int i = 0; i < n; i++)
-        game->insert(backup_players[i]);
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
+
+//inserção não ordenada por score
+void teste_1_2(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados\n";
+    Matchmaking* mm = gerar_jogo_desordenado(n_players);
+
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
+
+// ordenação via insertion sort por score sem empate
+void teste_2_1(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados sem empate\n";
     
-    cout << "\nOrdenando por Insertion e por Merge..." << endl;
+    Matchmaking* mm = gerar_jogo_desordenado(n_players);
+    mm->printWaitingPlayers();
 
-    cout << "\n\tInsertion: (resultado)" << endl;
+    cout << "\nOrdenando via insertion sort\n";
+    mm->sortByScoreInsertion();
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
 
-    game->sortByScoreInsertion();
-    game->printWaitingPlayers();
+// ordenação via insertion sort por score com empate
+void teste_2_2(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados com empate\n";
 
-    delete game;
-    game = new Matchmaking();
-    for (int i = 0; i < n; i++)
-        game->insert(backup_players[i]);
+    Matchmaking* mm = gerar_jogo_desordenado_empatado(n_players);
+    mm->printWaitingPlayers();
 
-    cout << "\n\tMerge: (resultado)" << endl;
+    cout << "\nOrdenando via insertion sort\n";
+    mm->sortByScoreInsertion();
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
 
-    game->sortByScoreMerge();
-    game->printWaitingPlayers();
+// ordenação via merge sort por score sem empate
+void teste_3_1(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados sem empate\n";
     
-    delete game;
-    game = new Matchmaking();
-    for (int i = 0; i < n; i++)
-        game->insert(backup_players[i]);
+    Matchmaking* mm = gerar_jogo_desordenado(n_players);
+    mm->printWaitingPlayers();
 
-    cout << "\nTestando grupo válido (size=3, delta=50)..." << endl;
+    cout << "\nOrdenando via merge sort\n";
+    mm->sortByScoreMerge();
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
 
-    game->sortByScoreMerge();
-    int size;
-    Player* grupo = game->formGroup(3, 50, &size);
-    game->printArrayPlayers(grupo, size, "Grupo removido");
-    delete[] grupo;
+// ordenação via merge sort por score com empate
+void teste_3_2(int n_players) {
+    cout << "\nGerando jogo com " << n_players << " jogadores não ordenados com empate\n";
 
-    cout << "Nova lista de espera: " << endl;
-    game->printWaitingPlayers();
+    Matchmaking* mm = gerar_jogo_desordenado_empatado(n_players);
+    mm->printWaitingPlayers();
+
+    cout << "\nOrdenando via merge sort\n";
+    mm->sortByScoreMerge();
+    mm->printWaitingPlayers();
+    delete mm;
+    return;
+}
+
+// seleção de grupo válida
+void teste_4_1(int n_players) {
+    int g_size = n_players/2;
+    int delta = n_players*5;
+
+    buscar_grupo(n_players, g_size, delta);
     
-    delete game;
-    game = new Matchmaking();
-    for (int i = 0; i < n; i++)
-        game->insert(backup_players[i]);
+    return;
+}
 
-    cout << "\nTestando grupo inválido (size=4, delta=20)..." << endl;
+//seleção de grupo inválida
+void teste_4_2(int n_players) {
+    int g_size = n_players/2;
+    int delta = n_players;
 
-    game->sortByScoreMerge();
-    grupo = game->formGroup(4, 20, &size);
-    game->printArrayPlayers(grupo, size, "Grupo removido");
-    delete[] grupo;
+    buscar_grupo(n_players, g_size, delta);
 
-    cout << "Nova lista de espera: " << endl;
-    game->printWaitingPlayers();
-    
-    delete game;
-    game = new Matchmaking();
-    for (int i = 0; i < n; i++)
-        game->insert(backup_players[i]);
+    return;
+}
 
-    cout << "Fim dos testes..." << endl;
-
-    delete game;
-    delete[] backup_players;
+int main(){
+    int n_players = 10;
+    teste_1_1(n_players);
+    teste_1_2(n_players);
+    teste_2_1(n_players);
+    teste_2_2(n_players);
+    teste_3_1(n_players);
+    teste_3_2(n_players);
+    teste_4_1(n_players);
+    teste_4_2(n_players);
 
     return 0;
 }
