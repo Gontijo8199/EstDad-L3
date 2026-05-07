b513f4c14442325b384e8dd006ee2d8bb158e4db
Rudá Brandão <rudabrandao@ruoso.com>
2026-05-06 21:20:25 -0300

    feat: formGroup v1

--- src/Matchmaking.cpp
+++ src/Matchmaking.cpp
@@ -99,13 +99,24 @@ void Matchmaking::sortByScoreMerge(){
     this->sortByScoreMerge(0, this->size - 1);
 }
 
-Player* Matchmaking::formGroup(int groupSize, int delta, int* n){
-    Player grupo[groupSize]; 
-    for (int i=0; i<this->size - groupSize; i++) {
+Player* Matchmaking::formGroup(int groupSize, int delta, int* n) {
+    this->sortByScoreMerge(); 
+    
+    Player* grupo = new Player[groupSize]; 
+    for (int i=0; i<this->size - groupSize; i++)
         if (this->players[i+groupSize].getScore() - this->players[i].getScore() < delta) {
-            int maxScore = this->players[i+groupSize].getScore();
+            for (int j=0; j<groupSize; j++) 
+                grupo[j] = this->players[i+j];
+
+            for (int j=0; j<groupSize; j++) 
+                this->removePlayer(this->players[i].getId());
+
+            *n = groupSize;
+            return grupo;
         }
-    }
+
+    *n = 0;
+    return nullptr;
 }
 
 Player* Matchmaking::getWaitingPlayers(int* n){
